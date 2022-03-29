b60e0d4adc0c7a6031c0f3b8eef5eab0d9abdc95
huanghe <2826944307@qq.com>
2022-03-29 20:31:40 +0800

    删除冗余导包代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,7 @@ package com.example.demo;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")
